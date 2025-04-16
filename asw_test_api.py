9a74c64334e4e6b9ff61a7b0e4323206ae6a01dd
Mathias Charconnet <mathias@OrdiMathias>
2025-04-16 15:11:22 +0200

    remove indentation

--- asw_test_api.py
+++ asw_test_api.py
@@ -1,43 +1,43 @@
-    user_aws = 'postgres'
-    password_aws = 'Ulk9froba'  
-    host_aws = 'raman-spectra-simulated.cst66uws8ol9.us-east-1.rds.amazonaws.com'
-    database_aws = 'Raman_simulated'
+user_aws = 'postgres'
+password_aws = 'Ulk9froba'  
+host_aws = 'raman-spectra-simulated.cst66uws8ol9.us-east-1.rds.amazonaws.com'
+database_aws = 'Raman_simulated'
 
     # Connect to the default 'postgres' database to create a new one
-    conn = psycopg2.connect(
-        dbname=database_aws,
-        user=user_aws,
-        password="Ulk9froba",  # replace with the password you set earlier
-        host=host_aws,
-        port = '5432'
-    )
+conn = psycopg2.connect(
+    dbname=database_aws,
+    user=user_aws,
+    password="Ulk9froba",  # replace with the password you set earlier
+    host=host_aws,
+    port = '5432'
+)
 
 
-    cur = conn.cursor()
+cur = conn.cursor()
 
 
-    query='SELECT * FROM "Raman_simulated_1"'
+query='SELECT * FROM "Raman_simulated_1"'
 
-    df_aws=pd.DataFrame(cur.fetchall())
+df_aws=pd.DataFrame(cur.fetchall())
 
-        compound = st.selectbox(
-            'Which chemical would you like to show raman spectra ?',
-            df_aws[0].unique())
-        concentration= st.selectbox(
-            'What concentration would you like to plot ?',
-            df_aws[1].unique())
+    compound = st.selectbox(
+        'Which chemical would you like to show raman spectra ?',
+        df_aws[0].unique())
+    concentration= st.selectbox(
+            What concentration would you like to plot ?',
+        df_aws[1].unique())
 
-    compound="Lactate"
-    concentration=0.5
+compound="Lactate"
+concentration=0.5
 
-    df_sub=df_aws[(df_aws[0] == 'Lactate') & (df_aws[1] == 0.5)]
+df_sub=df_aws[(df_aws[0] == 'Lactate') & (df_aws[1] == 0.5)]
 
-    fig, ax = plt.subplots(figsize=(12, 8))
-    ax.plot(df_sub[2], df_sub[3], label='Mixture', linewidth=2)
-    ax.set_xlabel("Raman shift (cm⁻¹)")
-    ax.set_ylabel("Intensity (a.u.)")
+fig, ax = plt.subplots(figsize=(12, 8))
+ax.plot(df_sub[2], df_sub[3], label='Mixture', linewidth=2)
+ax.set_xlabel("Raman shift (cm⁻¹)")
+ax.set_ylabel("Intensity (a.u.)")
         
-    st.pyplot(fig)
+st.pyplot(fig)
 
-    cur.close()
-    conn.close()
+cur.close()
+conn.close()
